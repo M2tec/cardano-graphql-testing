564e88cea02c7d96b819c89cab33c836b6ddf525
Maarten Menheere <m.menheere@m2tec.nl>
2025-10-01 16:56:39 +0200

    Add updates for Gamechanger transactions

--- test/gamechanger/transactions_orig.query.test.js
+++ test/gamechanger/transactions_orig.query.test.js
@@ -46,9 +46,7 @@ describe('transactions', () => {
   let client
 
   beforeAll(async () => {
-    client = await testClient.preprod()
-
-
+    client = await testClient.preprodOrig()
   });
 
   for (const address of addresList) {
@@ -58,21 +56,25 @@ describe('transactions', () => {
       console.log(`Querying transactions: ${address}`);
 
       const graphqlAddressData = await client.query({
-        query: await loadQueryNode('transactions_postgraphile_tip'),
+        query: await loadQueryNode('transactions_original'),
         variables: {
           "where": {
-            "transactionInputsByTxHash": {
-              "_some": {
-                "address": {
-                  "_in": [ address ]
-                }
+            "hash": {
+              "_in": [
+                      "ffe6ba4ba5c27d48496bc334c1f7e6d1f12fd95bcaf2d03637b23944bb63f017",
+                      "cc06efdd929dbf739893d9c88577f13f61edc0d1db9a0414106fb9d351ab33dc",           
+                      "34bb391769ee5e203a07ac3c4ed46372412e0faa1b471f2c5f8f8a3842348a13",
+                      "48e52efb0f8a363fbf052b8e6b59ef9f30a44f0bcac76e530bff82f9a7c7d51f"        
+                    ]
               }
-            }
+          },    
+          "order_by": {"includedAt": "asc"}
           }
-        }
-      })
 
-      util.saveResult(graphqlAddressData.data, "gamechanger", "transactions_postgraphile", `${address}_graphql.json`);
+        })
+
+
+      util.saveResult(graphqlAddressData.data, "gamechanger", "transactions_postgraphile", `${address}_orig.json`);
 
       // let gqlBlockfrostData = util.graphqlToBlockfrost(graphqlAddressData.data)
 
